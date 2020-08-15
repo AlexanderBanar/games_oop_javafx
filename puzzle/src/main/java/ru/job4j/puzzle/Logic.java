c4f71d45d76155d6944a4dd192a6d356588c52c4
AlexanderBanar <s595659@mail.ru>
2020-08-15 22:40:26 +0300

    Перенос методов в класс Win

--- puzzle/src/main/java/ru/job4j/puzzle/Logic.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Logic.java
@@ -60,50 +60,8 @@ public class Logic {
         return rst;
     }
 
-    public static int[] extractDiagonal(int[][] board) {
-        int[] rsl = new int[board.length];
-        for (int i = 0; i < board.length; i++) {
-            rsl[i] = board[i][i];
-        }
-        return rsl;
-    }
-
-    public static boolean monoVertical(int[][] board, int column) {
-        boolean result = true;
-        for (int i = 0; i < board.length; i++) {
-            if (board[i][column] != 1) {
-                result = false;
-                break;
-            }
-        }
-        return result;
-    }
-
-    public static boolean monoHorizontal(int[][] board, int row) {
-        boolean result = true;
-        for (int i = 0; i < board[row].length; i++) {
-            if (board[row][i] != 1) {
-                result = false;
-                break;
-            }
-        }
-        return result;
-    }
-
     public boolean isWin() {
-        int[][] table = this.convert();
-        boolean result = false;
-        int[] diagonal = extractDiagonal(table);
-        for (int i = 0; i < diagonal.length; i++) {
-            if (diagonal[i] == 1) {
-                if (monoHorizontal(table, i) || monoVertical(table, i)) {
-                    result = true;
-                    break;
-                }
-            }
-        }
-        //return result;
-         return Win.check(convert());
+        return Win.check(convert());
     }
 
     public int[][] convert() {
--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -3,6 +3,47 @@ package ru.job4j.puzzle;
 public class Win {
     public static boolean check(int[][] board) {
         boolean rsl = true;
+        int[][] table = Logic.convert();
+        boolean result = false;
+        int[] diagonal = extractDiagonal(table);
+        for (int i = 0; i < diagonal.length; i++) {
+            if (diagonal[i] == 1) {
+                if (monoHorizontal(table, i) || monoVertical(table, i)) {
+                    result = true;
+                    break;
+                }
+            }
+        }
         return rsl;
+    }
+
+    public static int[] extractDiagonal(int[][] board) {
+        int[] rsl = new int[board.length];
+        for (int i = 0; i < board.length; i++) {
+            rsl[i] = board[i][i];
+        }
+        return rsl;
+    }
+
+    public static boolean monoVertical(int[][] board, int column) {
+        boolean result = true;
+        for (int i = 0; i < board.length; i++) {
+            if (board[i][column] != 1) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+
+    public static boolean monoHorizontal(int[][] board, int row) {
+        boolean result = true;
+        for (int i = 0; i < board[row].length; i++) {
+            if (board[row][i] != 1) {
+                result = false;
+                break;
+            }
+        }
+        return result;
     }
 }

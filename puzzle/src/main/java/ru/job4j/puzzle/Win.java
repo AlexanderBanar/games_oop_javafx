fc3f77259615113d6081437387d0a331d24b903b
AlexanderBanar <s595659@mail.ru>
2020-08-17 21:11:26 +0300

    2. Сокобан. Тестовое задание. [#333425] _ correction 3

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -2,7 +2,6 @@ package ru.job4j.puzzle;
 
 public class Win {
     public static boolean check(int[][] board) {
-        boolean rsl = true;
         boolean result = false;
         int[] diagonal = extractDiagonal(board);
         for (int i = 0; i < diagonal.length; i++) {
@@ -13,7 +12,7 @@ public class Win {
                 }
             }
         }
-        return rsl;
+        return result;
     }
 
     public static int[] extractDiagonal(int[][] board) {
